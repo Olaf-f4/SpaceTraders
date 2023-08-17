ed1f9a670441e861e578f6ec7d0791355925d2b5
Olaf <olafsenb@yahoo.com>
2023-08-17 05:47:37 -0400

    Add files via upload

---
+++ src/main/java/Translate.java
@@ -0,0 +1,21 @@
+public class Translate {
+
+    private String name;
+    private String symbol;
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
